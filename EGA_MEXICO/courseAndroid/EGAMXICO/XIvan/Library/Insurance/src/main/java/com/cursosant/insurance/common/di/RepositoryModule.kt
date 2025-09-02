1b892b75b3a000d2715d24f37cc83d704ac48211
MemoMiura <guillermo.gonzalez@miurabox.com>
2025-09-02 14:41:48 -0600

    Inject UserService into QuoteFullRepository

--- EGA_MEXICO/courseAndroid/EGAMXICO/XIvan/Library/Insurance/src/main/java/com/cursosant/insurance/common/di/RepositoryModule.kt
+++ EGA_MEXICO/courseAndroid/EGAMXICO/XIvan/Library/Insurance/src/main/java/com/cursosant/insurance/common/di/RepositoryModule.kt
@@ -2,6 +2,7 @@ package com.cursosant.insurance.common.di
 
 import android.content.Context
 import com.cursosant.insurance.common.dataAccess.MultiQuoteService
+import com.cursosant.insurance.common.dataAccess.UserService
 import com.cursosant.insurance.common.utils.DateUtils
 import com.cursosant.insurance.deleteAccountModule.model.DeleteAccountRepository
 import com.cursosant.insurance.deleteAccountModule.model.DataSource as DeleteAccountDataSource
@@ -32,11 +33,13 @@ object RepositoryModule {
         @ApplicationContext context: Context,
         dataSource: QuoteFullDataSource,
         multiQuoteService: MultiQuoteService,
+        userService: UserService,
         date: DateUtils
     ): QuoteFullRepository = QuoteFullRepository(
         context = context,
         dataSource = dataSource,
         multiQuoteService = multiQuoteService,
+        userService = userService,
         date = date
     )
 }
