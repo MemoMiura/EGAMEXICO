aa37672bf6ce960e3460303a63ea2714418a833a
Jhazmin <jhazmin.alvarez@miurabox.com>
2025-10-15 16:04:55 -0600

    Support paged policy retrieval

--- EGA_MEXICO/courseAndroid/EGAMXICO/XIvan/Library/Insurance/src/main/java/com/cursosant/insurance/policiesModule/model/DataSource.kt
+++ EGA_MEXICO/courseAndroid/EGAMXICO/XIvan/Library/Insurance/src/main/java/com/cursosant/insurance/policiesModule/model/DataSource.kt
@@ -29,4 +29,19 @@ class DataSource @Inject constructor(private val service: MiuraboxService) {
     suspend fun getPoliciesPage(token: String, page: Int?): PoliciesPage {
         return service.getPoliciesPageByUser("${Constants.H_BEARER}$token", page)
     }
+
+    suspend fun getPolicies(
+        token: String,
+        username: String,
+        page: Int?,
+        pageSize: Int
+    ): PolicyPagedResponse {
+        val normalizedPageSize = pageSize.takeIf { it > 0 }
+        return service.getPoliciesPaged(
+            "${Constants.H_BEARER}$token",
+            username,
+            page,
+            normalizedPageSize
+        )
+    }
 }
