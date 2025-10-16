25a42f1567735973a60fb08baa23cc0025ffa500
Jhazmin <jhazmin.alvarez@miurabox.com>
2025-10-15 20:11:38 -0600

    Restore paging3 implementation for policies list

--- EGA_MEXICO/courseAndroid/EGAMXICO/XIvan/Library/Insurance/src/main/java/com/cursosant/insurance/policiesModule/view/PoliciesFragment.kt
+++ EGA_MEXICO/courseAndroid/EGAMXICO/XIvan/Library/Insurance/src/main/java/com/cursosant/insurance/policiesModule/view/PoliciesFragment.kt
@@ -42,7 +42,7 @@ class PoliciesFragment : Fragment(), OnClickListener {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentPoliciesBinding.inflate(inflater, container, false)
         return binding.root
@@ -71,7 +71,7 @@ class PoliciesFragment : Fragment(), OnClickListener {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(requireActivity())
             adapter = adapterWithFooter
-        }.also { adapter.setOnClickListener(this) }
+        }.also { adapter.setOnClickListener(this@PoliciesFragment) }
     }
 
     private fun setupObservers() {
@@ -138,10 +138,6 @@ class PoliciesFragment : Fragment(), OnClickListener {
     * OnClickListener
     * */
     override fun onClick(policy: Policy) {
-        /*val navController = requireActivity().findNavController(R.id.nav_host_fragment_content_main)
-        val action = PoliciesFragmentDirections.actionPoliciesToPolicyDetail()
-        action.idPolicy = policy.id
-        navController.navigate(action)*/
         navUtils.run {
             val args = Bundle()
             args.putLong(Constants.ARG_ID, policy.id)
